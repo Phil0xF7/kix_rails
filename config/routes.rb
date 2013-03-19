eb29ae4f763da1127445a28288c7ef7f29b7befa
Neilson Maknarakham <nmaknarakham@gmail.com>
2013-03-19 15:01:13 -0700

    Subject: Issue #2, login status Author: Neil -added login status and logout button -login status available in all views

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ KixRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'tasks#admin'
+  root :to => 'tasks#index'
 
   # See how all your routes lay out with "rake routes"
 
