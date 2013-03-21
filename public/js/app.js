28c918769cbae58cea8ed5fa494a33676eafaf9d
ichabodcole <ichabodcole@gmail.com>
2013-03-21 02:46:53 -0700

    Subject Button States to Tasks Author: Cole Commit: WIP Add button select states to task categories and sub tasks

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,30 @@
 $(function(){
   $('.cat-button').click(function(e){
-    $(this).parent().children('.sub-tasks').slideToggle(500, function(){
+    hideOtherCats();
+    $(this).addClass('cat-btn-select');
+    // $(this).children('.dotted').children('li').addClass('.dotted-select');
+
+    var subTask = $(this).parent().children('.sub-tasks');
+    $(subTask).slideToggle(500, function(){
+      if($(this).css('display') == 'none'){
+        $(e.target).removeClass('cat-btn-select');
+      }
     });
+  });
+
+  function hideOtherCats(){
+    $('.sub-tasks').each(function(index){
+      var catButton = $(this).parent().children('.cat-button');
+      if ($(this).css('display') == 'block'){
+        $(this).slideUp(500, function(){
+          $(catButton).removeClass('cat-btn-select');
+        });
+      }
+    });
+  }
+
+  $('.sub-task').click(function(){
+
   });
 
   $('.sub-tasks').hide();
