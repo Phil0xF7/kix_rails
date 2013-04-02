c299f4592526de5d87658d64eb8dc8c23975b5be
Phil0xF7 <phil@phamlabs.com>
2013-04-02 14:19:12 -0700

    clean up migration files
    -changed task_type to integer
    -deleted unneccessary migration file

--- db/migrate/20130318215450_create_tasks.rb
+++ db/migrate/20130318215450_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.integer :user_id
-      t.string :type_task
+      t.integer :type_task
       t.text :text
       t.datetime :completed
 
--- db/migrate/20130318225038_change_type_task_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTypeTaskType < ActiveRecord::Migration
-  def up
-    change_column :tasks, :type_task, :integer, :limit => nil
-  end
-
-  def down
-    change_column :tasks, :type_task, :string
-  end
-end
